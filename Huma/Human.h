0966107ed9c7c55787822f120e736927f87987fc
golog17 <golog17@29-15>
2025-09-08 13:09:33 +0300

    asd

--- Huma/Human.h
+++ Huma/Human.h
@@ -1,5 +1,22 @@
 #pragma once
 class Human
 {
-};
+private:
+	char* name;
+	char* surname;
+	int age;
+public:
+	Human();
+	Human(const char* name, const char* surname, int age);
+	~Human();
 
+	char* GetName();
+	char* GetSurname();
+	int GetAge();
+
+
+	void SetName(char* name);
+	void SetSurname(char* surname);
+	void SetAge(int age);
+
+};
